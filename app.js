18f9dd627835faef10dbd393761dea872a52ee22
Joe Laurole <Josue.laurole@gmail.com>
2021-05-26 12:04:05 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+
+import { lazy, Suspense } from 'react'
+import React, { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Routes } from './constants/routes'
+
+const Login = lazy(() => import('./pages/Authentication/login'))
+const Signup = lazy(() => import('./pages/Authentication/signup'))
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<p>I am Loading...</p>}>
+        <Switch>
+          <Route path={Routes.LOGIN} component={Login} />
+          <Route path={Routes.SIGNUP} component={Signup} />
+        </Switch>
+      </Suspense>
+    </BrowserRouter>
+  )
+}
+
+export default 
